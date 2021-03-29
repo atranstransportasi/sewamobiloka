4ca89908a41da549507ba3da699b209fd3a7bc24
edi-prasetyo <ediprasetiyo2@gmail.com>
2021-03-29 14:08:46 +0700

    Update Order Page

--- application/views/front/page/order_page.php
+++ application/views/front/page/order_page.php
@@ -17,23 +17,24 @@ $meta = $this->meta_model->get_meta();
                 <!-- <h2>Cara Order</h2> -->
                 <div class="row">
                     <div class="col-md-6">
-                <img class="img-fluid" src="<?php echo base_url('assets/img/galery/cara-order.png');?>"> 
-                </div>
-                <div class="col-md-6">
-                    <h3>Cara Order</h3>
-                    <div style="line-height: 40px;">
-                    <ol>
-                        <li>Pilih Produk pada menu Produk lalu pilih salah satu jenis Produk Antam atau Minigold</li>
-                        <li>Klik Order Produk, lalu klik tombol Beli emas</li>
-                        <li>Isi data diri anda, lalu klik Order sekarang</li>
-                        <li>Transfer Pembayaran</li>
-                        <li>Konfirmasi Pembayaran dengan menghubungi Nomor whatsaap <?php echo $meta->telepon;?> </li>
-                        <li>Produk akan di kirim ke data alamat yang anda input di point nomor 3  </li>
+                        <!-- <img class="img-fluid" src="<?php echo base_url('assets/img/galery/cara-order.png'); ?>"> -->
+                    </div>
+                    <div class="col-md-6">
+                        <h3>Cara Order</h3>
+                        <div style="line-height: 40px;">
+                            <ol>
+                                <li>Pilih Kota pada menu lalu pilih salah Kota destinasi anda</li>
+                                <li>Pilih Mobil yang akan anda sewa, lalu klik tombol Order</li>
+                                <li>Pilih tombol booking untuk melakukan order</li>
+                                <li>Isi semua data penyewa, lalu klik Order sekarang</li>
+                                <li>Transfer Pembayaran</li>
+                                <li>Konfirmasi Pembayaran dengan menghubungi Nomor whatsaap <?php echo $meta->telepon; ?> </li>
+                                <li>Setelah order di konfirmasi kami akan memberikan detail info driver yang akan menjemput </li>
 
-                    </ol>
+                            </ol>
+                        </div>
                     </div>
                 </div>
-                </div>          
             </div>
         </div>
     </div>
